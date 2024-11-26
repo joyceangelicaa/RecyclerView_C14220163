b24d1d6e27ac4b16b5628cd9e7dc53c18be8b0c8
joyceangelicaa <151369319+joyceangelicaa@users.noreply.github.com>
2024-11-26 11:23:27 +0700

    File 10.1 (RecyclerView)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "tugaskelas.c14220163.recyclerview_c14220163"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "tugaskelas.c14220163.recyclerview_c14220163"
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.picasso)
 }
---
+++ app/src/main/java/tugaskelas/c14220163/recyclerview_c14220163/wayang.kt
@@ -0,0 +1,8 @@
+package tugaskelas.c14220163.recyclerview_c14220163
+
+data class wayang(
+    var foto : String,
+    var nama : String,
+    var karakter : String,
+    var deskripsi : String
+)
